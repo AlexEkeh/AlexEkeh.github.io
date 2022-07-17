c8cea198c26835c6217a0ac9321a95f068a0ce9a
Decagon <decagon@Alexander.local>
2022-07-17 13:34:04 +0100

    Portfolio Done

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Developer", "Writer", "Researcher", "Freelancer"],
+        strings: ["Software Engineer", "Writer", "Researcher", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Developer", "Writer", "Researcher", "Freelancer"],
+        strings: ["Software Engineer", "Writer", "Researcher", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
